9281bcac59c748d10df762502f68f613fc7b111e
bolexpl <arek2159@wp.pl>
2021-05-06 00:39:31 +0200

    matlab

--- matlab/read_data.m
+++ matlab/read_data.m
@@ -3,10 +3,10 @@ resultFileID = fopen('result_file.txt','r');
 testTrainFileID = fopen('test_train_file.txt','r');
 testResultFileID = fopen('test_result_file.txt','r');
 
-train_data=fscanf(trainFileID, '%f %f %f %f %f %f', [6 Inf])';
-result_data=fscanf(resultFileID, '%f %f', [2 Inf])';
-test_train_data=fscanf(testTrainFileID, '%f %f %f %f %f %f', [6 Inf])';
-test_result_data=fscanf(testResultFileID, '%f %f', [2 Inf])';
+x=fscanf(trainFileID, '%f %f %f %f %f %f', [6 Inf])';
+y=fscanf(resultFileID, '%f %f', [2 Inf])';
+test_x=fscanf(testTrainFileID, '%f %f %f %f %f %f', [6 Inf])';
+test_y=fscanf(testResultFileID, '%f %f', [2 Inf])';
 
 fclose(trainFileID);
 fclose(resultFileID);
