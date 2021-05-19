a98bd941688e7a016821ed910cb112c19ddda7a1
bolexpl <arek2159@wp.pl>
2021-05-20 00:30:17 +0200

    matlab

--- matlab/read_data.m
+++ matlab/read_data.m
@@ -1,16 +1,13 @@
-trainFileID = fopen('x.txt','r');
-resultFileID = fopen('y.txt','r');
-godotFileID = fopen('y_godot.txt','r');
-matlabFileID = fopen('y_matlab.txt','r');
+trainFileID = fopen('data/x.txt','r');
+resultFileID = fopen('data/y.txt','r');
 
 x=fscanf(trainFileID, '%f %f %f %f %f %f', [6 Inf])';
 y=fscanf(resultFileID, '%f %f', [2 Inf])';
-y_godot=fscanf(godotFileID, '%f %f', [2 Inf])';
-y_matlab=fscanf(matlabFileID, '%f %f', [2 Inf])';
 
 fclose(trainFileID);
 fclose(resultFileID);
-fclose(godotFileID);
-fclose(matlabFileID);
-clear trainFileID resultFileID godotFileID matlabFileID
+clear trainFileID resultFileID
+
+% writetable(x,'plik.csv')
+
 
