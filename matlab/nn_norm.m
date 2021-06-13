34b39ee4c85cbd7b2980a085e03c829db36420a4
bolexpl <arek2159@wp.pl>
2021-06-13 13:54:12 +0200

    a

--- matlab/nn_norm.m
+++ matlab/nn_norm.m
@@ -9,4 +9,6 @@ end
 tmp = (x(:,6)/400) * (u - l) - l;
 n_x = [n_x tmp];
 
+dlmwrite('data/n_x.csv', n_x, ';')
+
 clear tmp i u l
--- matlab/read_data.m
+++ matlab/read_data.m
@@ -1,12 +1,6 @@
-trainFileID = fopen('data/x.csv','r');
-resultFileID = fopen('data/y.csv','r');
-
-x=fscanf(trainFileID, '%f;%f;%f;%f;%f;%f', [6 Inf])';
-y=fscanf(resultFileID, '%f;%f', [2 Inf])';
-
-fclose(trainFileID);
-fclose(resultFileID);
-clear trainFileID resultFileID ans
+x = dlmread('data/x.csv',';');
+n_x = dlmread('data/n_x.csv',';');
+y = dlmread('data/y.csv',';');
 
 % writetable(x,'plik.csv')
 
