853141ed28540d28bf03fb7e872869a7e1f7d1df
bolexpl <arek2159@wp.pl>
2021-08-16 19:12:01 +0200

    matlab

--- matlab/coder/create.m
+++ matlab/coder/create.m
@@ -7,71 +7,71 @@
 trainFcn = 'trainlm';  % Levenberg-Marquardt backpropagation.
 
 % tor 2
-x = x2;
-t = y2;
-hiddenLayerSize = 20;
-net = fitnet(hiddenLayerSize, trainFcn);
-net.divideParam.trainRatio = 70/100;
-net.divideParam.valRatio = 15/100;
-net.divideParam.testRatio = 15/100;
-[net,tr] = train(net,x,t);
-genFunction(net, 'gdNet2T20N.m', 'MatrixOnly','yes')
+% x = x2';
+% t = y2';
+% hiddenLayerSize = 20;
+% net = fitnet(hiddenLayerSize, trainFcn);
+% net.divideParam.trainRatio = 70/100;
+% net.divideParam.valRatio = 15/100;
+% net.divideParam.testRatio = 15/100;
+% [net,tr] = train(net,x,t);
+% genFunction(net, 'gdNet2T20N.m', 'MatrixOnly','yes')
 
-hiddenLayerSize = 30;
-net = fitnet(hiddenLayerSize, trainFcn);
-net.divideParam.trainRatio = 70/100;
-net.divideParam.valRatio = 15/100;
-net.divideParam.testRatio = 15/100;
-[net,tr] = train(net,x,t);
-genFunction(net, 'gdNet2T30N.m', 'MatrixOnly','yes')
+% hiddenLayerSize = 30;
+% net = fitnet(hiddenLayerSize, trainFcn);
+% net.divideParam.trainRatio = 70/100;
+% net.divideParam.valRatio = 15/100;
+% net.divideParam.testRatio = 15/100;
+% [net,tr] = train(net,x,t);
+% genFunction(net, 'gdNet2T30N.m', 'MatrixOnly','yes')
 
-hiddenLayerSize = 40;
-net = fitnet(hiddenLayerSize, trainFcn);
-net.divideParam.trainRatio = 70/100;
-net.divideParam.valRatio = 15/100;
-net.divideParam.testRatio = 15/100;
-[net,tr] = train(net,x,t);
-genFunction(net, 'gdNet2T40N.m', 'MatrixOnly','yes')
+% hiddenLayerSize = 40;
+% net = fitnet(hiddenLayerSize, trainFcn);
+% net.divideParam.trainRatio = 70/100;
+% net.divideParam.valRatio = 15/100;
+% net.divideParam.testRatio = 15/100;
+% [net,tr] = train(net,x,t);
+% genFunction(net, 'gdNet2T40N.m', 'MatrixOnly','yes')
 
 % oba tory
-x = x3;
-t = y3;
-hiddenLayerSize = 20;
-net = fitnet(hiddenLayerSize, trainFcn);
-net.divideParam.trainRatio = 70/100;
-net.divideParam.valRatio = 15/100;
-net.divideParam.testRatio = 15/100;
-[net,tr] = train(net,x,t);
-genFunction(net, 'gdNet3T20N.m', 'MatrixOnly','yes')
+% x = x3';
+% t = y3';
+% hiddenLayerSize = 20;
+% net = fitnet(hiddenLayerSize, trainFcn);
+% net.divideParam.trainRatio = 70/100;
+% net.divideParam.valRatio = 15/100;
+% net.divideParam.testRatio = 15/100;
+% [net,tr] = train(net,x,t);
+% genFunction(net, 'gdNet3T20N.m', 'MatrixOnly','yes')
 
-hiddenLayerSize = 30;
-net = fitnet(hiddenLayerSize, trainFcn);
-net.divideParam.trainRatio = 70/100;
-net.divideParam.valRatio = 15/100;
-net.divideParam.testRatio = 15/100;
-[net,tr] = train(net,x,t);
-genFunction(net, 'gdNet3T30N.m', 'MatrixOnly','yes')
+% hiddenLayerSize = 30;
+% net = fitnet(hiddenLayerSize, trainFcn);
+% net.divideParam.trainRatio = 70/100;
+% net.divideParam.valRatio = 15/100;
+% net.divideParam.testRatio = 15/100;
+% [net,tr] = train(net,x,t);
+% genFunction(net, 'gdNet3T30N.m', 'MatrixOnly','yes')
 
-hiddenLayerSize = 40;
-net = fitnet(hiddenLayerSize, trainFcn);
-net.divideParam.trainRatio = 70/100;
-net.divideParam.valRatio = 15/100;
-net.divideParam.testRatio = 15/100;
-[net,tr] = train(net,x,t);
-genFunction(net, 'gdNet3T40N.m', 'MatrixOnly','yes')
+% hiddenLayerSize = 40;
+% net = fitnet(hiddenLayerSize, trainFcn);
+% net.divideParam.trainRatio = 70/100;
+% net.divideParam.valRatio = 15/100;
+% net.divideParam.testRatio = 15/100;
+% [net,tr] = train(net,x,t);
+% genFunction(net, 'gdNet3T40N.m', 'MatrixOnly','yes')
 
 
-x2 = [x2; x2];
-y2 = [y2; y2];
-x3 = [x3; x3];
-y3 = [y3; y3];
-[x2, y2] = mix(x2, y2);
-[x3, y3] = mix(x3, y3);
+% x2 = [x2; x2];
+% y2 = [y2; y2];
+% x3 = [x3; x3];
+% y3 = [y3; y3];
+% [x2, y2] = mix(x2, y2);
+% [x3, y3] = mix(x3, y3);
 
 
 % tor 2 double
-x = x2;
-t = y2;
+x = x2';
+t = y2';
 hiddenLayerSize = 20;
 net = fitnet(hiddenLayerSize, trainFcn);
 net.divideParam.trainRatio = 70/100;
@@ -97,8 +97,8 @@ net.divideParam.testRatio = 15/100;
 genFunction(net, 'gdNet2T40ND.m', 'MatrixOnly','yes')
 
 % oba tory double
-x = x3;
-t = y3;
+x = x3';
+t = y3';
 hiddenLayerSize = 20;
 net = fitnet(hiddenLayerSize, trainFcn);
 net.divideParam.trainRatio = 70/100;
